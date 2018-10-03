71895fa1a0fc50c78604de00f52d6ff0d744d7ab
yasuomaidana <yasuomaidana@gmail.com>
2018-10-02 23:51:59 -0400

    1.2

--- setZaxis.py
+++ setZaxis.py
@@ -1,5 +1,6 @@
 from utilery import Reader
 from utilery import Writer
+import utilery as Tools
 R=Reader()
 print('You are in easy mode gCode set Z axis modifier')
 print('In order to use the easy mode, please put your files in the same carpet of the program')
@@ -12,7 +13,8 @@ print('------------------')
 if s==1:
     print('Easy Mode')
     print('Insert the name of your file')
-    p=input('(without .gcode) :')
+    #p=input('(without .gcode) :')
+    p='DPE_Tensile-8'
 elif s==2:
     print('Manual mode')
     print('Insert the path of your file, please be sure of ')
@@ -31,5 +33,7 @@ if s!=3:
         Read=R.oFullPath(p)
     B=Read.readlines()
     Read.close()
-    for i in B:
-        print (i)
+    #z=input('Enter the Z start value :')
+    z=1
+    print('The nearest value is')
+    print(Tools.obtainZWorkValues(B,z))
--- utilery.py
+++ utilery.py
@@ -20,6 +20,32 @@ def printGCodeLines(mA, start, end):
     for i in range(start, end):
         print(mA[i])
 
+def searchNearestValue(mA,Value):
+    import numpy as np
+    array = np.asanyarray(mA)
+    idx=(np.abs(array-Value)).argmin()
+    return mA[idx]
+
+def obtainZWorkValues(mA,Value):
+    mZ=[]
+    for i in mA:
+        if 'Z' in i:
+            if 'G28' not in i:
+                for j in i.split():
+                    if 'Z' in j:
+                        if j.replace('Z','') != '':
+                            mZ.append(float(j.replace('Z','')))
+    del mZ[:1]
+    import numpy
+    min=numpy.asanyarray(mZ)
+    return [searchNearestValue(mZ,Value),mZ[min.argmin()]]
+
+def newZstartModifier(mA,Value):
+    mR=[]
+    allow=0
+    #for i in mA:
+
+
 
 class Reader:
     #paths: str
